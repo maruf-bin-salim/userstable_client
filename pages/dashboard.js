a01b3da6f53e6461a93bfac48fd1feea034de130
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-09 18:20:29 +0600

    u

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,6 +6,7 @@ export default function Dashboard() {
 
     const [session, setSession] = useState(null);
     const router = useRouter();
+    const [email, setEmail] = useState(session?.user.email || '');
 
     useEffect(() => {
         const data = supabase.auth.onAuthStateChange((event, session) => {
@@ -15,6 +16,12 @@ export default function Dashboard() {
 
             if ((event === 'SIGNED_OUT' || event === 'INITIAL_SESSION') && !session) {
                 router.push('/');
+            }
+            else {
+                if (session) {
+                    setEmail(session?.user.email);
+                }
+                setSession(session);
             }
         });
 
@@ -30,6 +37,16 @@ export default function Dashboard() {
             {session && <div className="absolute top-0 right-0 p-4 bg-[#3b3b3b] text-white">User: {session.user.email}</div>}
             <div className="block min-w-[90vw] md:min-w-[300px] lg:min-w-[400px] bg-[#3b3b3b] p-4 rounded-[20px]">
                 <h1 className="text-white">Dashboard</h1>
+                <p>
+                    Update Email
+                </p>
+                <input type="email" onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <button onClick={async () => {
+                    const { data, error } = await supabase.auth.updateUser({ email: email });
+                    console.log(data, error);
+                }}>Update Email</button>
                 <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
             </div>
         </div>
